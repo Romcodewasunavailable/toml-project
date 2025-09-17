19cf4bb129e44abf94bfba5def212efb63c8a5da
Widmo206 <widmo206@yahoo.com>
2025-09-17 11:31:55 +0200

    implemented toml formatting

--- tomllib_test.py
+++ tomllib_test.py
@@ -12,8 +12,29 @@ import tomllib as tl
 def main():
     with open("example0.toml", "rb") as file:
         toml = tl.load(file)
-        print(toml)
+        print_data(toml)
 
+
+def format_dict(data: dict, leftpad: int=0) -> str:
+
+    result = ""
+    for key in data.keys():
+        value = data[key]
+
+        if type(value) is dict:
+            result += f"\n{leftpad*' '}[{key}]\n{format_dict(value, leftpad+2)}"
+
+        else:
+            result += f"{leftpad*' '}{key} = {value}\n"
+
+    return result
+
+
+
+
+def print_data(data: dict) -> None:
+    """Turn the data into a human-readable format and print to the console."""
+    print(format_dict(data))
 
 
 if __name__ == "__main__":
