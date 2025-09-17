b8d7dcf8271e7b9da7167b653295bbcda2f1a638
Widmo206 <widmo206@yahoo.com>
2025-09-17 15:52:42 +0200

    Added a real config file and updated the display code to better support lists
    The config file comes from this Minecraft mod: https://modrinth.com/mod/quark

--- tomllib_test.py
+++ tomllib_test.py
@@ -9,8 +9,12 @@ Created on 2025.09.17
 import tomllib as tl
 
 
+indent_depth = 2
+inline_list_max_length = 4
+
+
 def main():
-    with open("example0.toml", "rb") as file:
+    with open("quark-common.toml", "rb") as file:
         toml = tl.load(file)
         print_data(toml)
 
@@ -21,15 +25,19 @@ def format_dict(data: dict, leftpad: int=0) -> str:
     for key in data.keys():
         value = data[key]
 
+        padding = leftpad * ' '
+
         if type(value) is dict:
-            result += f"\n{leftpad*' '}[{key}]\n{format_dict(value, leftpad+2)}"
+            result += f"\n{padding}[{key}]\n{format_dict(value, leftpad+indent_depth)}"
+
+        elif type(value) is list and len(value) > inline_list_max_length:
+            list_padding = (leftpad + indent_depth) * ' '
+            result += f"{padding}{key} = [\n{',\n'.join([list_padding + repr(element) for element in value])}\n{padding}]\n"
 
         else:
-            result += f"{leftpad*' '}{key} = {value}\n"
+            result += f"{padding}{key} = {value}\n"
 
     return result
-
-
 
 
 def print_data(data: dict) -> None:
