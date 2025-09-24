37b2d2bd5bf92796193ecf3e5a56a9d268b8b107
Widmo206 <widmo206@yahoo.com>
2025-09-24 11:24:11 +0200

    Moved print_data() into a separate file

---
+++ display.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""Code for displaying formatted data in the console
+
+Moved into a separate file
+
+Created on 2025.09.24
+Contributors:
+    Jakub
+"""
+
+
+indent_depth = 2
+inline_list_max_length = 4
+
+
+def format_dict(data: dict, leftpad: int=0) -> str:
+
+    result = ""
+    for key in data.keys():
+        value = data[key]
+
+        padding = leftpad * ' '
+
+        if type(value) is dict:
+            result += f"\n{padding}[{key}]\n{format_dict(value, leftpad+indent_depth)}"
+
+        elif type(value) is list and len(value) > inline_list_max_length:
+            list_padding = (leftpad + indent_depth) * ' '
+
+            bs = ',\n' # needed because python < 3.12 is stupid
+            result += f"{padding}{key} = [\n{bs.join([list_padding + repr(element) for element in value])}\n{padding}]\n"
+
+        else:
+            result += f"{padding}{key} = {value}\n"
+
+    return result
+
+
+def print_data(data: dict) -> None:
+    """Turn the data into a human-readable format and print to the console."""
+    print(format_dict(data))
--- tomllib_test.py
+++ tomllib_test.py
@@ -8,10 +8,7 @@ Contributors:
 
 
 import tomllib as tl
-
-
-indent_depth = 2
-inline_list_max_length = 4
+from display import print_data
 
 
 def main():
@@ -19,31 +16,6 @@ def main():
         toml = tl.load(file)
         print_data(toml)
 
-
-def format_dict(data: dict, leftpad: int=0) -> str:
-
-    result = ""
-    for key in data.keys():
-        value = data[key]
-
-        padding = leftpad * ' '
-
-        if type(value) is dict:
-            result += f"\n{padding}[{key}]\n{format_dict(value, leftpad+indent_depth)}"
-
-        elif type(value) is list and len(value) > inline_list_max_length:
-            list_padding = (leftpad + indent_depth) * ' '
-            result += f"{padding}{key} = [\n{',\n'.join([list_padding + repr(element) for element in value])}\n{padding}]\n"
-
-        else:
-            result += f"{padding}{key} = {value}\n"
-
-    return result
-
-
-def print_data(data: dict) -> None:
-    """Turn the data into a human-readable format and print to the console."""
-    print(format_dict(data))
 
 
 if __name__ == "__main__":
