ad911de09fe24227b468df14ac09b6e3946bc9aa
Romain <rlaou08@gmail.com>
2025-10-15 11:30:11 +0200

    Update character.py

--- character.py
+++ character.py
@@ -5,7 +5,9 @@ Contributors:
     Romain
 """
 
-from dataclasses import dataclass
+from __future__ import annotations
+from dacite import from_dict
+from dataclasses import asdict, dataclass
 
 
 @dataclass(frozen=True)
@@ -16,8 +18,40 @@ class Vector2():
 
 
 @dataclass(frozen=True)
+class Item():
+    """Inventory filling material"""
+    name: str
+
+
+@dataclass(frozen=True)
 class Character():
     """Save data for a video game character"""
     name: str
-    birth_year: int
+    age: int
     position: Vector2
+    inventory: dict[Item, int]
+
+    @classmethod
+    def test_character(cls) -> Character:
+        """Sample character with arbitrary attributes for testing"""
+        return cls(
+            "Bob",
+            21,
+            Vector2(42.0, -3.14),
+            {
+                Item("Apple"): 3,
+                Item("Sword"): 1,
+                Item("Healing Potion"): 2,
+                Item("Jakub ;)"): 1,
+            }
+        )
+    
+    @classmethod
+    def from_dict(cls, data: dict) -> Character:
+        """"""
+        return from_dict(cls, data)
+
+    def to_dict(self) -> dict:
+        """"""
+        return asdict(self)
+        
